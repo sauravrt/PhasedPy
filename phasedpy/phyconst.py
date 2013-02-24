fcd607d70a025c3bb8013b1d79a1a73b09117dc9
Saurav R Tuladhar <sauravtuladhar@gmail.com>
2013-02-23 21:09:27 -0500

    Added avg. sound speed in seawater.

--- phasedpy/phyconst.py
+++ phasedpy/phyconst.py
@@ -2,5 +2,8 @@
 Constant values useful for as reference """
 
 class PhysicalConst(object):
+    ''' Physical constants related to the propagation envirnoment
+    All quantities in metric unit.'''
     SPEED_LIGHT = 3e8
     SPEED_SOUND_AIR = 340.29
+    SPEED_SOUND_SEA = 1560    # Wikipedia
